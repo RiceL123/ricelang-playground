143ce5cdfbec81ed2bd9ee8025a989620251af87
Eric L <115810538+RiceL123@users.noreply.github.com>
2025-03-29 14:28:53 +1100

    Parser (#2)

--- test.sh
+++ test.sh
@@ -2,13 +2,13 @@ test_file=$1
 temp_file="temp.out"
 
 echo "diffing java VC.vc $test_file.vc with $test_file"
-javac vc.java && java VC.vc Recogniser/$test_file.vc > $temp_file
+javac vc.java && java VC.vc Parser/$test_file.vc > $temp_file
 echo '====================== RECEIVED ======================'
 cat $temp_file
 
 echo '====================== EXPECTED ======================'
-cat Recogniser/$test_file.sol
+cat Parser/$test_file.sol
 
 echo '====================== DIFF ======================'
-diff $temp_file Recogniser/$test_file.sol
+diff $temp_file Parser/$test_file.sol
 rm $temp_file
