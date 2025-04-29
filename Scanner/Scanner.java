e1c236bf6b34c66b28b246390085633e54877c8e
Eric L <115810538+RiceL123@users.noreply.github.com>
2025-04-29 12:00:00 +1000

    token granularity

--- Scanner/Scanner.java
+++ Scanner/Scanner.java
@@ -73,34 +73,8 @@ public final class Scanner {
         currentChar = sourceFile.getNextChar();
     }
 
-    private void accept_noSpellingAppend() {
-        switch (currentChar) {
-            case '\r' -> {
-                if (sourceFile.inspectChar(1) == '\n') {
-                    sourceFile.getNextChar(); // consume & ignore next char if '\r\n' found
-                }
-                line += 1;
-                column = 1;
-            }
-            case '\n' -> {
-                line += 1;
-                column = 1;
-            }
-            default ->
-                column += 1;
-        }
-
-        currentChar = sourceFile.getNextChar();
-    }
-
     private void reject() {
         column += 1;
-        currentChar = sourceFile.getNextChar();
-    }
-
-    private void reject_start() {
-        line += 1;
-        column = 1;
         currentChar = sourceFile.getNextChar();
     }
 
@@ -124,108 +98,108 @@ public final class Scanner {
         return sourceFile.inspectChar(nthChar);
     }
 
-    private int nextToken() {
+    private Token nextToken() {
         // Tokens: separators, operators, literals, identifiers, and keywords
         switch (currentChar) {
             case '(' -> {
                 accept();
-                return Token.LPAREN;
+                return new Token(Token.LPAREN, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case ')' -> {
                 accept();
-                return Token.RPAREN;
+                return new Token(Token.RPAREN, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '{' -> {
                 accept();
-                return Token.LCURLY;
+                return new Token(Token.LCURLY, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '}' -> {
                 accept();
-                return Token.RCURLY;
+                return new Token(Token.RCURLY, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '[' -> {
                 accept();
-                return Token.LBRACKET;
+                return new Token(Token.LBRACKET, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case ']' -> {
                 accept();
-                return Token.RBRACKET;
+                return new Token(Token.RBRACKET, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case ';' -> {
                 accept();
-                return Token.SEMICOLON;
+                return new Token(Token.SEMICOLON, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case ',' -> {
                 accept();
-                return Token.COMMA;
+                return new Token(Token.COMMA, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '+' -> {
                 accept();
-                return Token.PLUS;
+                return new Token(Token.PLUS, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '-' -> {
                 accept();
-                return Token.MINUS;
+                return new Token(Token.MINUS, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '*' -> {
                 accept();
-                return Token.MULT;
+                return new Token(Token.MULT, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '/' -> {
                 accept();
-                return Token.DIV;
+                return new Token(Token.DIV, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '=' -> {
                 accept();
                 if (currentChar == '=') {
                     accept();
-                    return Token.EQEQ;
+                    return new Token(Token.EQEQ, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 }
-                return Token.EQ;
+                return new Token(Token.EQ, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '!' -> {
                 accept();
                 if (currentChar == '=') {
                     accept();
-                    return Token.NOTEQ;
+                    return new Token(Token.NOTEQ, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 }
-                return Token.NOT;
+                return new Token(Token.NOT, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '<' -> {
                 accept();
                 if (currentChar == '=') {
                     accept();
-                    return Token.LTEQ;
+                    return new Token(Token.LTEQ, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 }
-                return Token.LT;
+                return new Token(Token.LT, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '>' -> {
                 accept();
                 if (currentChar == '=') {
                     accept();
-                    return Token.GTEQ;
+                    return new Token(Token.GTEQ, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 }
-                return Token.GT;
+                return new Token(Token.GT, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             }
             case '&' -> {
                 accept();
                 if (currentChar == '&') {
                     accept();
-                    return Token.ANDAND;
+                    return new Token(Token.ANDAND, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 }
-                return Token.ERROR; // there is no bitwise & in VC; only &&
+                return new Token(Token.ERROR, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1)); // there is no bitwise & in VC; only &&
             }
             case '|' -> {
                 accept();
                 if (currentChar == '|') {
                     accept();
-                    return Token.OROR;
+                    return new Token(Token.OROR, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 }
-                return Token.ERROR; // there is no bitwise | in VC; only ||
+                return new Token(Token.ERROR, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1)); // there is no bitwise | in VC; only ||
             }
             case SourceFile.eof -> {
                 currentSpelling.append(Token.spell(Token.EOF));
-                return Token.EOF;
+                return new Token(Token.EOF, currentSpelling.toString(), new SourcePosition(line, line, column, column));
             }
         }
 
@@ -247,8 +221,8 @@ public final class Scanner {
 
                 accept();
             }
-            return Token.FLOATLITERAL;
 
+            return new Token(Token.FLOATLITERAL, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
         } else if (Character.isDigit(currentChar)) {
             accept();
 
@@ -271,7 +245,7 @@ public final class Scanner {
 
                         accept();
                     }
-                    return Token.FLOATLITERAL;
+                    return new Token(Token.FLOATLITERAL, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 } else if (isExponent()) {
                     // skip the (E|e) and the (+|-)?digit then read rest of digit*
                     accept();
@@ -279,7 +253,7 @@ public final class Scanner {
                     while (Character.isDigit(currentChar)) {
                         accept();
                     }
-                    return Token.FLOATLITERAL;
+                    return new Token(Token.FLOATLITERAL, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 } else if (currentChar == '.') {
                     // can't be a fraction so we are checking if it ends with . or ends with . exponent
                     accept();
@@ -292,11 +266,11 @@ public final class Scanner {
                             accept();
                         }
                     }
-                    return Token.FLOATLITERAL;
+                    return new Token(Token.FLOATLITERAL, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 } else if (Character.isDigit(currentChar)) {
                     accept();
                 } else {
-                    return Token.INTLITERAL;
+                    return new Token(Token.INTLITERAL, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 }
             }
         } else if (currentChar == '\"') {
@@ -305,9 +279,14 @@ public final class Scanner {
                 if (currentChar == '\n' || currentChar == '\r') {
                     // strings do not span multiple lines
                     currentSpelling = new StringBuilder(currentSpelling.toString().replaceFirst("^\"", "")); // remove first and last " 
-                    errorReporter.reportError("%: unterminated string", currentSpelling.toString(), sourcePos);
-                    reject_start();
-                    return Token.STRINGLITERAL;
+                    errorReporter.reportError("%: unterminated string", currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length() - 1, column - currentSpelling.length() - 1));
+                    int temp_line = line;
+                    int temp_column = column;
+                    reject();
+                    column = 1;
+                    line += 1;
+                    // return Token.STRINGLITERAL;
+                    return new Token(Token.STRINGLITERAL, currentSpelling.toString(), new SourcePosition(temp_line, temp_line, temp_column - currentSpelling.length() - 1 - spellingLengthBonus, temp_column - 1));
                 } else if (currentChar == '\\') {
                     char nextChar = inspectChar(1);
                     Optional<Character> special = switch (nextChar) {
@@ -338,7 +317,7 @@ public final class Scanner {
                         reject();
                         spellingLengthBonus += 1;
                     } else {
-                        errorReporter.reportError("%: illegal escape character", "\\" + nextChar, sourcePos);
+                        errorReporter.reportError("%: illegal escape character", "\\" + nextChar, new SourcePosition(line, line, column - currentSpelling.length(), column));
                         accept();
                         accept();
                     }
@@ -350,8 +329,10 @@ public final class Scanner {
 
             }
             accept();
-            currentSpelling = new StringBuilder(currentSpelling.toString().replaceFirst("^\"", "").replaceFirst("\"$", "")); // remove first and last " 
-            return Token.STRINGLITERAL;
+            currentSpelling = new StringBuilder(currentSpelling.toString().replaceFirst("^\"", "").replaceFirst("\"$", "")); // remove first and last "
+
+            // althought the first and last "" have disappeared, they still needa be there for position calc 
+            return new Token(Token.STRINGLITERAL, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length() - 2 - spellingLengthBonus, column - 1));
         } else if (Character.isLetter(currentChar)) {
             // identifier -> letter (letter | digit)*
             while (Character.isLetter(currentChar) || Character.isDigit(currentChar)) {
@@ -359,42 +340,42 @@ public final class Scanner {
             }
             return switch (currentSpelling.toString()) {
                 case "boolean" ->
-                    Token.BOOLEAN;
+                    new Token(Token.BOOLEAN, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "break" ->
-                    Token.BREAK;
+                    new Token(Token.BREAK, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "continue" ->
-                    Token.CONTINUE;
+                    new Token(Token.CONTINUE, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "else" ->
-                    Token.ELSE;
+                    new Token(Token.ELSE, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "float" ->
-                    Token.FLOAT;
+                    new Token(Token.FLOAT, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "for" ->
-                    Token.FOR;
+                    new Token(Token.FOR, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "if" ->
-                    Token.IF;
+                    new Token(Token.IF, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "int" ->
-                    Token.INT;
+                    new Token(Token.INT, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "return" ->
-                    Token.RETURN;
+                    new Token(Token.RETURN, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "void" ->
-                    Token.VOID;
+                    new Token(Token.VOID, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "while" ->
-                    Token.WHILE;
+                    new Token(Token.WHILE, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 case "true", "false" ->
-                    Token.BOOLEANLITERAL;
+                    new Token(Token.BOOLEANLITERAL, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
                 default ->
-                    Token.ID;
+                    new Token(Token.ID, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
             };
         }
 
         accept();
-        return Token.ERROR;
+        return new Token(Token.ERROR, currentSpelling.toString(), new SourcePosition(line, line, column - currentSpelling.length(), column - 1));
     }
 
     private void skipSpaceAndComments() {
         while (Character.isWhitespace(currentChar) || (currentChar == '/' && inspectChar(1) == '*') || (currentChar == '/' && inspectChar(1) == '/')) {
             // skip tab but make sure it multiple of 8 aligned (A tab size of 8 characters is assumed)
-            if (currentChar == '\t') {
+            while (currentChar == '\t') {
                 accept();
                 for (int i = column; i % 8 != 0; i++) {
                     column++;
@@ -445,7 +426,6 @@ public final class Scanner {
 
     public Token getToken() {
         Token token;
-        int kind;
 
         // Skip white space and comments and reset thingos
         skipSpaceAndComments();
@@ -453,20 +433,7 @@ public final class Scanner {
         spellingLengthBonus = 0;
         sourcePos = new SourcePosition();
 
-        kind = nextToken();
-
-        if (kind == Token.EOF) {
-            sourcePos = new SourcePosition(line, line, column, column);
-        } else if (kind == Token.STRINGLITERAL) {
-            // position includes the quotes ever though spelling removes them
-            sourcePos = new SourcePosition(line, line, column - (currentSpelling.toString().length() + 2 + spellingLengthBonus), column - 1);
-        } else if (currentSpelling.toString().length() == 1) {
-            sourcePos = new SourcePosition(line, line, column - 1, column - 1);
-        } else {
-            sourcePos = new SourcePosition(line, line, column - currentSpelling.toString().length(), column - 1);
-        }
-
-        token = new Token(kind, currentSpelling.toString(), sourcePos);
+        token = nextToken();
 
         // * do not remove these three lines below (for debugging purposes)
         if (debug) {
