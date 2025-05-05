cb7254d92f400ff49b031ee9c0542f1a4a788781
Eric L <115810538+RiceL123@users.noreply.github.com>
2025-05-05 12:00:00 +1000

    Checker (#3)

---
+++ tests/Checker/everything/errors/11e.sol
@@ -0,0 +1,7 @@
+======= The VC compiler =======
+
+Pass 1: Lexical and syntactic Analysis
+Pass 2: Semantic Analysis
+ERROR: 4(9)..4(9): *11: attempt to use an array/function as a scalar
+ERROR: 5(9)..5(9): *11: attempt to use an array/function as a scalar
+Compilation was unsuccessful.
---
+++ tests/Checker/failure/whateva.sol
@@ -0,0 +1,8 @@
+======= The VC compiler =======
+
+Pass 1: Lexical and syntactic Analysis
+Pass 2: Semantic Analysis
+ERROR: 2(13)..2(13): *18: array size missing
+ERROR: 3(17)..3(17): *11: attempt to use an array/function as a scalar
+ERROR: 3(17)..3(21): *9: incompatible type for this binary operator
+Compilation was unsuccessful.
