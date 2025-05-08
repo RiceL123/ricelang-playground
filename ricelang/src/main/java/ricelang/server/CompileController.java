802c2d546a39a38cd143fcadba4183cd501a310a
Eric L <115810538+RiceL123@users.noreply.github.com>
2025-05-08 12:00:00 +1000

    frontend

--- ricelang/src/main/java/ricelang/server/CompileController.java
+++ ricelang/src/main/java/ricelang/server/CompileController.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import ricelang.VC.vc;
 import jasmin.Main;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class CompileController {
 
     @PostMapping("/compile")
