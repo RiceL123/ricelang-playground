c234d9ff9f238258955304a0957aa649269af7df
RiceL123 <115810538+RiceL123@users.noreply.github.com>
2025-06-05 00:36:04 +1000

    🍚

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🍚</text></svg>" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-dvh text-xs sm:text-base`}
