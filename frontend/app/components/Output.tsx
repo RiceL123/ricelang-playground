628497a264e52b6360a9fc3d381f86eebb4307d7
RiceL123 <115810538+RiceL123@users.noreply.github.com>
2025-07-06 00:11:24 +1000

    👻

--- frontend/app/components/Output.tsx
+++ frontend/app/components/Output.tsx
@@ -1,12 +1,7 @@
-import React from "react";
 import Loading from "./Loading";
 import Mermaid from "./Mermaid";
-
-interface OutputProp {
-  isAST: boolean;
-  output: string;
-  verbose: string;
-}
+import { useAtom } from "jotai";
+import { loadingAtom, readOutputAtom } from "@/lib/jotai";
 
 function Separator({ text }: { text: string }) {
   return (
@@ -15,24 +10,33 @@ function Separator({ text }: { text: string }) {
       <span className="flex-shrink mx-4 text-muted-foreground">{text}</span>
       <div className="flex-grow border-t border-muted-foreground"></div>
     </div>
-  )
+  );
 }
 
-const Output = ({ output, loading }: { output: OutputProp, loading: boolean }) => {
+const Output = () => {
+  const [loading] = useAtom(loadingAtom);
+  const [output] = useAtom(readOutputAtom);
+
   return (
-    <div className="h-full w-full max-w-full max-h-full flex p-4 overflow-auto rounded-xl bg-primary-foreground/20 backdrop-blur-[4px] border border-2 border-accent-foreground shadow-sm hover:bg-primary-foreground/30 transition">
-      {loading
-        ? <Loading message="Compiling..." />
-        : output.isAST
-          ? <Mermaid mermaidSrc={output.output} />
-          : <div className="w-full h-full">
-            {output.verbose && output.verbose !== "" && (<><Separator text="Verbose" /><pre >{output.verbose}</pre></>)}
-            <Separator text="Output" />
-            <pre>{output.output}</pre>
-          </div>
-      }
-    </div >
-  )
-}
+    <div className="h-full w-full max-w-full max-h-full flex p-4 overflow-auto rounded-xl bg-primary-foreground/20 backdrop-blur-[4px] border-2 border-accent-foreground shadow-sm hover:bg-primary-foreground/30 transition">
+      {loading ? (
+        <Loading message="Compiling..." />
+      ) : output.isAST ? (
+        <Mermaid mermaidSrc={output.output} />
+      ) : (
+        <div className="w-full h-full">
+          {output.verbose && output.verbose !== "" && (
+            <>
+              <Separator text="Verbose" />
+              <pre>{output.verbose}</pre>
+            </>
+          )}
+          <Separator text="Output" />
+          <pre>{output.output}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default React.memo(Output);
+export default Output;
